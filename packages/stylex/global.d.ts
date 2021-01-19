f3b5facf144ba242e0ebc587c985caa50d7ecb5d
somibuon <somibuon1234@gmail.com>
2021-01-19 20:45:41 +0700

    Update declaration files (#5)
    * Update index.d.ts
    
    * Update global.d.ts
    
    * Update index.d.ts
    
    * Update global.d.ts
    
    * Update index.d.ts
    
    * resolve
    
    * fix disallow unknow properties
    
    Co-authored-by: Hoang Duc <anhduc.hang@vmodev.com>

--- packages/stylex/global.d.ts
+++ packages/stylex/global.d.ts
@@ -16,17 +16,24 @@ type NormalCssProperties = css.Properties<string | number>
 type NormalCssValues<K> = K extends keyof NormalCssProperties
   ? NormalCssProperties[K] | CssValue
   : CssValue
+type NormalCssStyle = {
+  [K in keyof NormalCssProperties]: NormalCssValues<K> | CssStyle
+}
 
-  export type CssStyle =
+/**
+ * This interface can be augmented by users to add default types for custom css properties when
+ * using `@ladifire-opensource/stylex/global`.
+ * Use module augmentation to append your own type definition in a your_custom_type.d.ts file.
+ * https://www.typescriptlang.org/docs/handbook/declaration-merging.html#module-augmentation
+ */
+interface CustomCssProperties extends NormalCssStyle {
+}
+
+export type CssStyle =
+  | NormalCssStyle
+  | CustomCssProperties
   | {
-      [K in keyof NormalCssProperties]:
-        | NormalCssValues<K>
-        | CssStyle
-    }
-  | {
-      [K: string]:
-        | CssValue
-        | CssStyle
+      [K: string]: NormalCssStyle | CssStyle
     }
 
 export type Style<Name extends string | number | symbol = string> = Record<
