94027dc67c3b297b35001dd3c4d09c8c2b0e1715
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-01-21 20:31:41 +0100

    Add better type description

--- packages/stylex/global.d.ts
+++ packages/stylex/global.d.ts
@@ -4,9 +4,9 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import * as CSS from 'csstype';
+import * as CSS from "csstype";
 
-export interface CSSProperties extends CSS.Properties<string | number> {
+export interface CSSProperties extends CSS.Properties<number | string> {
   /**
    * The index signature was removed to enable closed typing for style
    * using CSSType. You're able to use type assertion or module augmentation
@@ -17,135 +17,12 @@ export interface CSSProperties extends CSS.Properties<string | number> {
    */
 }
 
-export type AdvancedPseudos =
-  | ":-moz-any()"
-  | ":-moz-dir"
-  | ":-webkit-any()"
-  | "::cue"
-  | "::cue-region"
-  | "::part"
-  | "::slotted"
-  | ":dir"
-  | ":has"
-  | ":host"
-  | ":host-context"
-  | ":is"
-  | ":lang"
-  | ":matches()"
-  | ":not"
-  | ":nth-child"
-  | ":nth-last-child"
-  | ":nth-last-of-type"
-  | ":nth-of-type"
-  | ":where";
-
-export type SimplePseudos =
-  | ":-khtml-any-link"
-  | ":-moz-any-link"
-  | ":-moz-focusring"
-  | ":-moz-full-screen"
-  | ":-moz-placeholder"
-  | ":-moz-read-only"
-  | ":-moz-read-write"
-  | ":-ms-fullscreen"
-  | ":-ms-input-placeholder"
-  | ":-webkit-any-link"
-  | ":-webkit-full-screen"
-  | "::-moz-placeholder"
-  | "::-moz-progress-bar"
-  | "::-moz-range-progress"
-  | "::-moz-range-thumb"
-  | "::-moz-range-track"
-  | "::-moz-selection"
-  | "::-ms-backdrop"
-  | "::-ms-browse"
-  | "::-ms-check"
-  | "::-ms-clear"
-  | "::-ms-fill"
-  | "::-ms-fill-lower"
-  | "::-ms-fill-upper"
-  | "::-ms-input-placeholder"
-  | "::-ms-reveal"
-  | "::-ms-thumb"
-  | "::-ms-ticks-after"
-  | "::-ms-ticks-before"
-  | "::-ms-tooltip"
-  | "::-ms-track"
-  | "::-ms-value"
-  | "::-webkit-backdrop"
-  | "::-webkit-input-placeholder"
-  | "::-webkit-progress-bar"
-  | "::-webkit-progress-inner-value"
-  | "::-webkit-progress-value"
-  | "::-webkit-slider-runnable-track"
-  | "::-webkit-slider-thumb"
-  | "::after"
-  | "::backdrop"
-  | "::before"
-  | "::cue"
-  | "::cue-region"
-  | "::first-letter"
-  | "::first-line"
-  | "::grammar-error"
-  | "::marker"
-  | "::placeholder"
-  | "::selection"
-  | "::spelling-error"
-  | ":active"
-  | ":after"
-  | ":any-link"
-  | ":before"
-  | ":blank"
-  | ":checked"
-  | ":default"
-  | ":defined"
-  | ":disabled"
-  | ":empty"
-  | ":enabled"
-  | ":first"
-  | ":first-child"
-  | ":first-letter"
-  | ":first-line"
-  | ":first-of-type"
-  | ":focus"
-  | ":focus-visible"
-  | ":focus-within"
-  | ":fullscreen"
-  | ":hover"
-  | ":in-range"
-  | ":indeterminate"
-  | ":invalid"
-  | ":last-child"
-  | ":last-of-type"
-  | ":left"
-  | ":link"
-  | ":only-child"
-  | ":only-of-type"
-  | ":optional"
-  | ":out-of-range"
-  | ":placeholder-shown"
-  | ":read-only"
-  | ":read-write"
-  | ":required"
-  | ":right"
-  | ":root"
-  | ":scope"
-  | ":target"
-  | ":valid"
-  | ":visited";
-
-export type Pseudos = AdvancedPseudos | SimplePseudos;
-
 // TODO: missing media query
-export type CSSPropertiesWithNestedPseudo<T extends string> = {
-  [key in T]: CSSProperties
-} | {
-  [key in T]: {
-    [key in Pseudos]?: CSSProperties
-  }
-}
+export type CSSPropertiesWithNestedPseudo<T extends string | number> = {
+  [key in T]: CSSObject;
+};
 
-export type CSSObject = CSSProperties & {
-  [key in Pseudos]?: CSSProperties
-}
-
+export type CSSObject = CSSProperties &
+  {
+    [key in CSS.Pseudos]?: CSSProperties;
+  };
--- packages/stylex/index.d.ts
+++ packages/stylex/index.d.ts
@@ -10,136 +10,123 @@
 // TODO: Hàm create dưới đây là đoạn mình viết, nhưng nó vẫn chưa thực sự chuẩn với mọi trường hợp, nên cũng cần review lại
 // TODO: (Khi viết type definition nhớ để ý pseudo, media query nhé)
 
-import {CSSPropertiesWithNestedPseudo, CSSObject, CSSProperties} from './global';
-
+import {
+  CSSPropertiesWithNestedPseudo,
+  CSSObject,
+  CSSProperties
+} from "./global";
 // tslint:disable-next-line:export-just-namespace
 export = stylex;
 export as namespace stylex;
 
-declare function stylex<T extends string>(...style: (CSSObject | CSSObject[])[]): string;
+/**
+ * stylex(...)
+ * Return css class(es) from stylex object(s)
+ *
+ * Example:
+ *   // Single stylex object
+ *   const styleRoot = stylex(styles.root)
+ *
+ *   // Multiple stylex objects
+ *   const multiStyle = stylex(styles.root, styles.container, ....)
+ *
+ *   // Array of stylex objects
+ *   const arrayStyle = stylex([styles.root, styles.container, ....])
+ */
+declare function stylex<T extends string>(
+  ...style: (CSSObject | CSSObject[])[]
+): string;
 
 declare namespace stylex {
-  // TODO 1: phần này chưa có definition
-  // vd 1: className={stylex(styles.root)} <== truyền 1 object stylex
-  // vd 2: className={stylex([styles.root, styles.container, ....])} <== truyền 1 mảng các objects stylex
+  /*
+   * stylex.create(obj1, obj2, ...)
+   * Return a dictionary of stylex objects
+   *
+   * Example
+   * const styles = stylex.create({
+   *   root: {
+   *     alignItems: 'center',
+   *     display: 'flex',
+   *   },
+   * });
+   *
+   * // Later on, styles Object can be used as
+   * <div className={stylex(styles.root)}>Hello</div>
+   * <div className={styles("root")}>Hello</div>
+   *
+   * TODO: Remove below note
+   * note:
+   *    In the above example, I've mentioned how we use this function, but it is
+   *    still vague. Perhaps this function should be better described.
+   */
+  function create<T extends string | number>(
+    styles: CSSPropertiesWithNestedPseudo<T>
+  ): { [key in T]: CSSObject } & ((...styles: T[]) => string);
 
-  // TODO 2: stylex.create(...)
-  // vd:
-  // const styles = stylex.create({
-  //   root: {
-  //     alignItems: 'center',
-  //     cursor: 'pointer',
-  //     display: 'flex',
-  //     marginTop: 0,
-  //     marginRight: 12,
-  //     marginBottom: 0,
-  //     marginLeft: 8,
-  //   },
-  // });
-  function create<T extends string | number>(styles: CSSPropertiesWithNestedPseudo<T>):
-    { [key in T]: CSSProperties } & (
-    (...styles: (
-      | keyof T
-      | boolean
-      | undefined
-      | null
-      | { [key in keyof T]?: boolean | undefined | null }
-      )[]) => string
-    )
+  /*
+   * Dedupe stylex object based on conditions and return a string contains css class names.
+   * stylex.dedupe(obj1, obj2)
+   *
+   * Example
+   *     stylex.dedupe({
+   *       display: "flex",
+   *       flexDirection: "row",
+   *       flexWrap: "wrap"
+   *     }, d === "narrow" ? {
+   *       margin: -2,
+   *     } : null, d === "wide" ? {
+   *       margin: -3,
+   *     } : null)
+   */
+  function dedupe(...styles: CSSObject[]): string;
 
-  // TODO 3: stylex.dedupe(...)
-  // Hàm này dedupe (có thể hiểu là sẽ lấy các giá trị sau đè lên các giá trị trước) các dối tượng theo một điều kiện nào đó
-  // kết quả trả về là một chuỗi string gồm các className ("aadsdfs etrewtre asdfs454s wer1werterw5 ....")
-  // Cấu trúc: vd1
-  //     stylex.dedupe({
-  //       display: "flex",
-  //       flexDirection: "row",
-  //       flexWrap: "wrap"
-  //     }, d === "narrow" ? {
-  //       margin: -2,
-  //     } : null, d === "wide" ? {
-  //       margin: -3,
-  //     } : null)
-  //
-  // ví dụ khác:
-  //
-  // <div
-  //   className={stylex.dedupe(
-  //     {
-  //       position: 'absolute',
-  //       top: 0,
-  //       left: 0,
-  //       bottom: 0,
-  //       right: 0,
-  //       transitionTimingFunction: 'var(--lf-animation-fade-out)',
-  //       pointerEvents: 'none',
-  //       transitionDuration: 'var(--lf-duration-extra-extra-short-out)',
-  //       transitionProperty: 'opacity',
-  //       cursor: 'pointer',
-  //     },
-  //     !isLink ? {
-  //       backgroundColor: 'var(--hover-overlay)',
-  //     } : null,
-  //     !isLink && (isHovered || selected) ? {
-  //       opacity: 1,
-  //     } : {
-  //       opacity: 0,
-  //     },
-  //     !isLink && isPressed ? {
-  //       backgroundColor: 'var(--press-overlay)',
-  //     } : null,
-  //     !isLink && isPressed && variant === 'primary-deemphasized' ? {
-  //       backgroundColor: 'var(--primary-deemphasized-button-pressed-overlay)',
-  //     } : null,
-  //     rounded ? {
-  //       borderRadius: '50%',
-  //     } : {
-  //       borderRadius: 6,
-  //     },
-  //   )}
-  // />
-  function dedupe(...styles: CssStyle[]): string;
+  /*
+   * Merge multiple stylex object
+   *
+   * Example:
+   *   stylex.compose(
+   *     {
+   *      //... stylex object 1
+   *     },
+   *     {
+   *       //... stylex object 2
+   *     }
+   *  )
+   */
+  function compose(...styles: CSSObject[]): CSSObject;
 
-  // TODO 4: stylex.compose(...)
-  // Hàm này merge các đối tượng stylex lại với nhau (kết quả là một đối tượng stylex duy nhất)
-  // Hàm này tương tự như Object.assign
-  // <div
-  //   className={stylex.compose(
-  //     {
-  //      //... stylex object 1
-  //     },
-  //     {
-  //       //... stylex object 1
-  //     }
-  //   )};
-  // />
-  function compose(...styles: CssStyle[]): CssStyle;
+  /*
+   * stylex.keyframes(...)
+   * Declare a keyframes animation name
+   *
+   * Example:
+   * let j = stylex.create({
+   *   root: {
+   *     animationDirection: "alternate",
+   *     animationDuration: "1s",
+   *     animationIterationCount: "infinite",
+   *     animationName: stylex.keyframes({
+   *       '0%': {
+   *         opacity: 0.25,
+   *       },
+   *       '100%': {
+   *         opacity: 1,
+   *       },
+   *     }),
+   *     animationTimingFunction: "steps(10,end)",
+   *     backgroundColor: "var(--wash)",
+   *     opacity: 0.25
+   *   }
+   * });
+   */
+  function keyframes(rules: CSSObject): string;
 
-  // TODO 5: stylex.keyframes(...)
-  // Hàm này khai báo một keyframes animation name
-  // Ví dụ:
-  // let j = stylex.create({
-  //   root: {
-  //     animationDirection: "alternate",
-  //     animationDuration: "1s",
-  //     animationIterationCount: "infinite",
-  //     animationName: stylex.keyframes({
-  //       '0%': {
-  //         opacity: 0.25,
-  //       },
-  //       '100%': {
-  //         opacity: 1,
-  //       },
-  //     }),
-  //     animationTimingFunction: "steps(10,end)",
-  //     backgroundColor: "var(--wash)",
-  //     opacity: 0.25
-  //   }
-  // });
-  function keyframes(rules: CssStyle): string;
-
-  // TODO 6: stylex.inject(...)
-  // inject một chuỗi css vào compiled js
-  // vd: inject(".a55dalm2{background-color:red}")
+  /*
+   * stylex.inject(str)
+   * Inject css into compiled js
+   *
+   * Example:
+   *    inject(".a55dalm2{background-color:red}")
+   */
   function inject(cssString: string): void;
 }
